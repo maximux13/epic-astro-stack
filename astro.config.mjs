318ae5671fc79fbbe7f45bc1e8efb0b3e22b2b2c
Andres Alarcon <aalarcon@monoku.com>
2024-01-27 18:09:21 -0500

    feat: add sentry and spotlightjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import icon from "astro-icon";
+import sentry from "@sentry/astro";
 import tailwind from "@astrojs/tailwind";
+import spotlightjs from "@spotlightjs/astro";
 import { squooshImageService, defineConfig } from "astro/config";
 
 import svgoOptions from "./svgo.config";
@@ -19,5 +21,15 @@ export default defineConfig({
     icon({
       svgoOptions,
     }),
+    sentry({
+      release: process.env.SENTRY_RELEASE,
+      sourceMapsUploadOptions: {
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        org: process.env.SENTRY_ORG,
+        project: process.env.SENTRY_PROJECT,
+        telemetry: false,
+      },
+    }),
+    spotlightjs(),
   ],
 });
