c924fe4be000e2ea7886a1f2d4572a479f18df4e
Andres Alarcon <aalarcon@monoku.com>
2024-01-29 15:04:15 -0500

    feat: add github actions

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,12 @@ import { getViteConfig } from "astro/config";
 
 export default getViteConfig({
   test: {
+    coverage: {
+      all: true,
+      include: ["src/**/*.{ts,astro,js}"],
+    },
     environment: "jsdom",
+    restoreMocks: true,
     setupFiles: ["vitest-setup.ts"],
   },
 });
